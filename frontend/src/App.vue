9fdb0843f1953421daaae23f050cb06943f0e29e
Dobby <56114680+Doyeon-Kang@users.noreply.github.com>
2022-09-05 02:37:50 +0900

    Navbar 컴포넌트 생성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <nav>
-  </nav>
-  <router-view/>
+  <Navbar></Navbar>
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navbar from "@/components/NavbarCom.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    Navbar,
+  },
+};
+</script>
---
+++ frontend/src/components/NavbarCom.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>navbar</h1>
+</template>

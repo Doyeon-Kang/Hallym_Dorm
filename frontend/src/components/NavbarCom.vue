8fc7f27aacfe4c540c732f7971b7a480c53c9871
Dobby <56114680+Doyeon-Kang@users.noreply.github.com>
2022-09-05 02:50:17 +0900

    less loader 설치

--- frontend/src/components/NavbarCom.vue
+++ frontend/src/components/NavbarCom.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>navbar</h1>
+  <header>
+    <div class="white_content">ds</div>
+    <div class="blue_content">ds</div>
+  </header>
 </template>
+
+<script></script>
+
+<style scoped>
+header {
+  background-color: #fff;
+}
+</style>

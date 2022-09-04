621134f5f0642e61f4b5ca3b0f327d7a7afd6643
Dobby <56114680+Doyeon-Kang@users.noreply.github.com>
2022-09-05 03:26:56 +0900

    navbar left logo 생성 및 라우터 연결

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,7 +18,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="less">
+@import "@/style/style.css";
+
 * {
   margin: 0;
   padding: 0;
--- frontend/src/components/NavbarCom.vue
+++ frontend/src/components/NavbarCom.vue
@@ -1,14 +1,53 @@
 <template>
   <header>
-    <div class="white_content">ds</div>
-    <div class="blue_content">ds</div>
+    <div class="white_container">
+      <router-link to="/" class="left">
+        <div class="logo">
+          <img src="@/assets/logo.png" alt="한림대학교 로고" />
+          <img src="@/assets/univ.png" alt="한림대학교 슬로건" />
+        </div>
+        <div class="bar"></div>
+        <div class="dorm">Dormitory</div>
+      </router-link>
+    </div>
+    <div class="blue_container"></div>
   </header>
 </template>
 
 <script></script>
 
-<style scoped>
+<style scoped lang="less">
 header {
-  background-color: #fff;
+  background-color: white;
+  .white_container {
+    height: 30px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    .left {
+      display: flex;
+      align-items: center;
+      cursor: left;
+      .logo {
+        margin-right: 4px;
+      }
+      .bar {
+        width: 1.5px;
+        height: 70%;
+        background: #858585;
+        margin-right: 6px;
+      }
+      .dorm {
+        font-size: 20px;
+        font-weight: 700;
+        color: #222222;
+      }
+    }
+  }
+  .blue_container {
+    background-color: #3675c7;
+    height: 50px;
+    padding: 10px;
+  }
 }
 </style>

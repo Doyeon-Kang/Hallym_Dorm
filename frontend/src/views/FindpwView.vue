7510ae77b3cb4510fac167b516f15d7b0a06a1a9
kunmin <rjsals0328@gmail.com>
2022-09-13 18:55:05 +0900

    size 수정

--- frontend/src/views/FindpwView.vue
+++ frontend/src/views/FindpwView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="findpw">
       <div class="findpwbox">
-        <div class="logo">
+        <div class="logo" @click="this.$router.push('/')">
           <img src="@/assets/logo.png" alt="" />
           <img src="@/assets/univ.png" alt="" />
           <div class="univ">| Dormitory</div>
@@ -17,32 +17,38 @@
         </div>
         <div class="findpwguide">※가입하신 이메일로 임시 비밀번호를 전송합니다.</div>
         <div class="findpwbtnbox findpwbtn">확인</div>
+        <div class="loginbtn" @click="this.$router.push('login')">◀ 로그인 페이지</div>
+        <div class="homebtn" @click="this.$router.push('/')">홈페이지 ▶</div>
       </div>
     </div>
   </template>
   
   <style scoped lang="less">
   .findpw {
-    width: 825px;
-    height: 830px;
-    margin: 200px auto;
-    box-shadow: 0px 0px 15px #bababa;
+    width: 625px;
+    height: 630px;
+    margin: 100px auto;
+    box-shadow: 0px 0px 10px #bababa;
     color: #222222;
   
     .findpwbox {
-      width: 525px;
-      height: 700px;
+      width: 400px;
       margin: 0 auto;
-      padding-top: 130px;
+      padding-top: 80px;
   
       .logo {
-        width: 280px;
+        width: 190px;
         margin: 0 auto;
         margin-bottom: 10px;
-  
+
+        img{
+          width:60px;
+        }
+
         .univ {
-          font-size: 20px;
+          font-size: 12px;
           font-weight: bold;
+          line-height: 40px;
           float: right;
         }
       }
@@ -51,19 +57,19 @@
         clear: both;
         color: #336EB4;
         text-align: center;
-        font-size: 50px;
+        font-size: 40px;
         font-weight: bold;
-        margin-bottom: 70px;
+        margin-bottom: 60px;
       }
   
       .findpw_id,.findpw_email {
         color: #00B6AD;
-        font-size: 18px;
+        font-size: 17px;
         font-weight: bold;
       }
   
       .findpw_id {
-        margin-bottom: 50px;
+        margin-bottom: 30px;
       }
   
       .findpw_email {
@@ -71,41 +77,61 @@
       }
   
       .findpw_text {
-        margin-bottom: 10px;
+        margin-bottom: 5px;
       }
   
       input {
-        width: 520px;
-        height: 40px;
+        width: 396px;
+        height: 30px;
         border: none;
         border-bottom: 2px solid #00B6AD;
-        font-size: 17px;
+        font-size: 14px;
       }
   
       .findpwguide{
         color: #858585;
         font-weight: bold;
+        font-size: 13px;
         float: left;
       }
   
       .findpwbtnbox {
         clear: both;
-        width: 525px;
-        height: 65px;
-        font-size: 26px;
+        width: 400px;
+        height: 50px;
+        font-size: 18px;
         font-weight: bold;
         text-align: center;
-        line-height: 65px;
+        line-height: 50px;
       }
   
-      .findpwbtnbox:hover {
+      .logo:hover, .findpwbtnbox:hover {
         cursor: pointer;
       }
   
       .findpwbtn {
         background-color: #00B6AD;
         color: #FFFFFF;
-        margin-top: 100px;
+        margin-top: 70px;
+      }
+      
+      .loginbtn, .homebtn{
+          color: #858585;
+          font-weight: bold;
+          margin-top: 10px;
+          font-size: 13px;
+        }
+
+        .loginbtn{
+          float: left;
+        }
+        
+        .homebtn{
+          float: right;
+        }
+
+      .homebtn:hover, .loginbtn:hover{
+        cursor: pointer;
       }
     }
   }

f6cdf3ad4a3db7cc70b2f8ba9c94d7ca3385797a
kunmin <rjsals0328@gmail.com>
2022-09-20 19:40:44 +0900

    login

--- frontend/src/views/JoinView.vue
+++ frontend/src/views/JoinView.vue
@@ -1,143 +1,158 @@
 <template>
-    <div class="join">
-        <div class="joinbox">
-            <div class="logo" @click="this.$router.push('/')">
-                <img src="@/assets/logo.png" alt="" />
-                <img src="@/assets/univ.png" alt="" />
-                <div class="univ">| Dormitory</div>
+    <div class="join_container">
+        <div class="join">
+            <div class="joinbox">
+                <div class="logo" @click="this.$router.push('/')">
+                    <img src="@/assets/logo.png" alt="" />
+                    <img src="@/assets/univ.png" alt="" />
+                    <div class="univ">| Dormitory</div>
+                </div>
+                <div class="jointitle">회원가입</div>
+                <div class="join_name">
+                    <div class="join_text">이름</div>
+                    <input type="text" v-model="name" placeholder="이름을 입력해주세요.">
+                </div>
+                <div class="join_id">
+                    <div class="join_text">아이디</div>
+                    <input type="text" v-model="id" placeholder="아이디를 입력해주세요.">
+                    <div class="joinguide">※아이디는 한림대학교 학번과 동일하게 입력.</div>
+                </div>
+                <div class="join_pw">
+                    <div class="join_text">비밀번호</div>
+                    <input type="password" v-model="pwd" placeholder="비밀번호를 입력해주세요.">
+                    <div class="joinguide">※비밀번호는 8자리 이상.</div>
+                </div>
+                <div class="join_pw_check">
+                    <div class="join_text">비밀번호 확인</div>
+                    <input type="password" v-model="pwd_check" placeholder="비밀번호를 다시 한 번 입력해주세요.">
+                </div>
+                <div class="join_email">
+                    <div class="join_text">이메일</div>
+                    <input type="text" v-model="email" placeholder="이메일을 입력해주세요.">
+                </div>
+                <div class="joinbtnbox joinbtn">회원가입</div>
+                <div class="loginbtn" @click="this.$router.push('login')">◀ 로그인 페이지</div>
+                <div class="homebtn" @click="this.$router.push('/')">홈페이지 ▶</div>
             </div>
-            <div class="jointitle">회원가입</div>
-            <div class="join_name">
-                <div class="join_text">이름</div>
-                <input type="text" v-model="name" placeholder="이름을 입력해주세요.">
-            </div>
-            <div class="join_id">
-                <div class="join_text">아이디</div>
-                <input type="text" v-model="id" placeholder="아이디를 입력해주세요.">
-                <div class="joinguide">※아이디는 한림대학교 학번과 동일하게 입력.</div>
-            </div>
-            <div class="join_pw">
-                <div class="join_text">비밀번호</div>
-                <input type="password" v-model="pwd" placeholder="비밀번호를 입력해주세요.">
-                <div class="joinguide">※비밀번호는 8자리 이상.</div>
-            </div>
-            <div class="join_pw_check">
-                <div class="join_text">비밀번호 확인</div>
-                <input type="password" v-model="pwd_check" placeholder="비밀번호를 다시 한 번 입력해주세요.">
-            </div>
-            <div class="join_email">
-                <div class="join_text">이메일</div>
-                <input type="text" v-model="email" placeholder="이메일을 입력해주세요.">
-            </div>
-            <div class="joinbtnbox joinbtn">회원가입</div>
-            <div class="loginbtn" @click="this.$router.push('login')">◀ 로그인 페이지</div>
-            <div class="homebtn" @click="this.$router.push('/')">홈페이지 ▶</div>
         </div>
     </div>
 </template>
   
 <style scoped lang="less">
-.join {
-    width: 625px;
-    height: 850px;
-    margin: 60px auto;
-    box-shadow: 0px 0px 10px #bababa;
-    color: #222222;
+.join_container {
+    width: 1080px;
 
-    .joinbox {
-        width: 400px;
-        margin: 0 auto;
-        padding-top: 60px;
+    .join {
+        width: 625px;
+        height: 850px;
+        margin: 60px auto;
+        box-shadow: 0px 0px 10px #bababa;
+        color: #222222;
 
-        .logo {
-            width: 190px;
+        .joinbox {
+            width: 400px;
             margin: 0 auto;
-            margin-bottom: 10px;
+            padding-top: 60px;
 
-            img{
-                width:60px;
+            .logo {
+                width: 190px;
+                margin: 0 auto;
+                margin-bottom: 10px;
+
+                img {
+                    width: 60px;
+                }
+
+                .univ {
+                    font-size: 12px;
+                    font-weight: bold;
+                    line-height: 40px;
+                    float: right;
+                }
             }
 
-            .univ {
-                font-size: 12px;
+            .jointitle {
+                clear: both;
+                color: #336EB4;
+                text-align: center;
+                font-size: 40px;
                 font-weight: bold;
-                line-height: 40px;
+                margin-bottom: 50px;
+            }
+
+            .join_name,
+            .join_id,
+            .join_pw,
+            .join_pw_check,
+            .join_email {
+                color: #00B6AD;
+                font-size: 17px;
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+
+            .join_id,
+            .join_pw {
+                margin-bottom: 40px;
+            }
+
+            .join_text {
+                margin-bottom: 5px;
+            }
+
+            input {
+                width: 396px;
+                height: 30px;
+                border: none;
+                border-bottom: 2px solid #00B6AD;
+                margin-bottom: 3px;
+                font-size: 14px;
+            }
+
+            .joinguide {
+                color: #DD6464;
+                font-size: 13px;
+                float: left;
+            }
+
+            .joinbtnbox {
+                clear: both;
+                width: 400px;
+                height: 50px;
+                font-size: 18px;
+                font-weight: bold;
+                text-align: center;
+                line-height: 50px;
+            }
+
+            .logo:hover,
+            .joinbtnbox:hover,
+            .loginbtn:hover,
+            .homebtn:hover {
+                cursor: pointer;
+            }
+
+            .joinbtn {
+                background-color: #00B6AD;
+                color: #FFFFFF;
+                margin-top: 50px;
+            }
+
+            .loginbtn,
+            .homebtn {
+                color: #858585;
+                font-weight: bold;
+                margin-top: 10px;
+                font-size: 13px;
+            }
+
+            .loginbtn {
+                float: left;
+            }
+
+            .homebtn {
                 float: right;
             }
-        }
-
-        .jointitle {
-            clear: both;
-            color: #336EB4;
-            text-align: center;
-            font-size: 40px;
-            font-weight: bold;
-            margin-bottom: 50px;
-        }
-
-        .join_name,.join_id,.join_pw,.join_pw_check,.join_email {
-            color: #00B6AD;
-            font-size: 17px;
-            font-weight: bold;
-            margin-bottom: 20px;
-        }
-
-        .join_id, .join_pw {
-            margin-bottom: 40px;
-        }
-
-        .join_text {
-            margin-bottom: 5px;
-        }
-
-        input {
-            width: 396px;
-            height: 30px;
-            border: none;
-            border-bottom: 2px solid #00B6AD;
-            margin-bottom: 3px;
-            font-size: 14px;
-        }
-
-        .joinguide{
-            color: #DD6464;
-            font-size: 13px;
-            float: left;
-        }
-
-        .joinbtnbox {
-            clear: both;
-            width: 400px;
-            height: 50px;
-            font-size: 18px;
-            font-weight: bold;
-            text-align: center;
-            line-height: 50px;
-        }
-
-        .logo:hover, .joinbtnbox:hover, .loginbtn:hover, .homebtn:hover{
-            cursor: pointer;
-        }
-
-        .joinbtn {
-            background-color: #00B6AD;
-            color: #FFFFFF;
-            margin-top: 50px;
-        }
-
-        .loginbtn, .homebtn{
-            color: #858585;
-            font-weight: bold;
-            margin-top: 10px;
-            font-size: 13px;
-        }
-
-        .loginbtn{
-            float: left;
-        }
-        
-        .homebtn{
-            float: right;
         }
     }
 }

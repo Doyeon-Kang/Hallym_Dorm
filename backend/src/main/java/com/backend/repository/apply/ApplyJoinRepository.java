3b3bb6f9653ecf7f149f00828a45c1c0f1906094
Hinatoy <l.akemino@yandex.ru>
2022-10-31 18:45:00 +0900

    more changes

--- backend/src/main/java/com/backend/repository/apply/ApplyJoinRepository.java
+++ backend/src/main/java/com/backend/repository/apply/ApplyJoinRepository.java
@@ -1,9 +1,11 @@
 package com.backend.repository.apply;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.model.apply.ApplyJoin;
 
 public interface ApplyJoinRepository extends JpaRepository<ApplyJoin, Long> {
-    ApplyJoin findByUserId(Long userId);
+    Optional<ApplyJoin> findByUserId(Long userId);
 }

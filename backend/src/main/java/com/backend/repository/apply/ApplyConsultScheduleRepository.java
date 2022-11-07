064abf8bb353c527995fe6ed0f4805305d2c3abf
soyeon-jeong <jee052@naver.com>
2022-11-07 19:29:10 +0900

    service fix

--- backend/src/main/java/com/backend/repository/apply/ApplyConsultScheduleRepository.java
+++ backend/src/main/java/com/backend/repository/apply/ApplyConsultScheduleRepository.java
@@ -1,11 +1,8 @@
 package com.backend.repository.apply;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.model.apply.ApplyConsultSchedule;
 
 public interface ApplyConsultScheduleRepository extends JpaRepository<ApplyConsultSchedule, Long> {
-    Optional<ApplyConsultSchedule> findByUserId(Long userId);
 }
--- backend/src/main/java/com/backend/repository/apply/ApplyStudyroomScheduleRepository.java
+++ backend/src/main/java/com/backend/repository/apply/ApplyStudyroomScheduleRepository.java
@@ -1,11 +1,8 @@
 package com.backend.repository.apply;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.model.apply.ApplyStudyroomSchedule;
 
 public interface ApplyStudyroomScheduleRepository extends JpaRepository<ApplyStudyroomSchedule, Long> {
-    Optional<ApplyStudyroomSchedule> findByUserId(Long userId);
 }

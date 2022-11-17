1400316d8d1f2273b2ae87fc4e9a79d724a1c27f
soyeon-jeong <jee052@naver.com>
2022-11-17 15:55:39 +0900

    no commit

--- backend/src/main/java/com/backend/repository/board/BoardNoticeRepository.java
+++ backend/src/main/java/com/backend/repository/board/BoardNoticeRepository.java
@@ -10,4 +10,6 @@ import com.backend.model.board.BoardNotice;
 public interface BoardNoticeRepository extends JpaRepository<BoardNotice, Long>  {
     List<BoardNotice> findByNotice1True();
     List<BoardNotice> findByNotice1False();
+    List<BoardNotice> fineByWriterStudentNoAndNoticeFalse(String studentNo);
+    List<BoardNotice> fineByWriterStudentNoAndNoticeTrue(String studentNo);
 }
